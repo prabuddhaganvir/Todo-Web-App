9e4981527f3f253c9a8c315252682fca5b4926b0
prabuddhaganvir <prabuddhapg12@gmail.com>
2025-06-03 12:47:45 +0530

    update frontend

--- Backend/server.js
+++ Backend/server.js
@@ -7,15 +7,17 @@ import connectDB from './config/db.js';
 
 
 
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.use(cors());
 app.use(express.json());
 app.use("/api/", todoRoutes)
+
+
 
 app.listen(PORT, () => {
     connectDB()
