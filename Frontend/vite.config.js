555a4bfb959c45e417923e173c857071a62f1c94
prabuddhaganvir <prabuddhapg12@gmail.com>
2025-06-03 12:54:42 +0530

    fix proxy error

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://todo-web-app-backend-ynim.onrender.com',
+        target: 'http://localhost:5001', // Adjust the target URL to your backend server
         changeOrigin: true,
       },
     },
